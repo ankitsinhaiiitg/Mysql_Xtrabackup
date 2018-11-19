689f9da07682b081750bcdd7080e3c0495fbef22
Ankit <as@zaubacorp.com>
2018-11-19 18:47:56 +0530

    clrf to lf

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -ex
 
 echo "Starting listener for backup requests"
-#/usr/bin/xtrabackup_nc.sh >> /tmp/xtrabackup_nc.log &
+/usr/bin/xtrabackup_nc.sh >> /tmp/xtrabackup_nc.log &
 
-ncat --listen --keep-open --send-only --max-conns=1 3307 -c \
-"xtrabackup --backup --stream=xbstream --user=root --password=not-so-secure"
+#ncat --listen --keep-open --send-only --max-conns=1 3307 -c \
+#"xtrabackup --backup --stream=xbstream --user=root --password=not-so-secure"
